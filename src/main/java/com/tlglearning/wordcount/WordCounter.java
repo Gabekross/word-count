aa878907d9237ad9489564e4661745ec25c68499
Gabekross <olugabriel80@gmail.com>
2022-07-21 15:09:06 -0700

    line at a time VS single string.

--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -5,49 +5,49 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public final class WordCounter {
+public class WordCounter {
 
-  private final Map<String, Integer> counts;
+  private final Map<String, Integer> counts = new HashMap<>();
+
+  private  int totalWords;
 
 
-  public WordCounter(String text) {
-   String [] words = splitWords(text);
-    counts = Collections.unmodifiableMap(countWords(words)); //Makes counts unmodifiable
-
-    }
+//  public WordCounter(String text) {
+//   String [] words = splitWords(text);
+//    counts = Collections.unmodifiableMap(countWords(words)); //Makes counts unmodifiable
+//
+//    }
 
   public Set<String> words (){
     return counts.keySet();
   }
 
-  public int getCount(String word){
+  public int get(String word){
     return counts.getOrDefault(word ,0);
   }
 
   public Map<String, Integer> getCounts() {
-    return counts;
+    return Collections.unmodifiableMap(counts);
   }
 
-  //Business/Helper Methods
-  //Method to countWords
-  Map<String, Integer> countWords(String[] words) {
-    Map<String, Integer> counts = new HashMap<>();
-    for (String word : words){
-
-      //DONE CHECK if word is already present as a key in counts
-      // if its not presnet, add it to counts with a value of 1;
-      // otherwise, get the current value, add 1 to it , and update the map with the new value
-      if (!counts.containsKey(word)){
-        counts.put(word, 1);
-      }else{
-        int previousCount = counts.get(word);
-        counts.put(word, previousCount +1);
-      }
+  public void add (String text){
+    String trimmedLine = text.trim();
+    if (!trimmedLine.isEmpty()) {
+      String [] words = splitWords(trimmedLine);
+      countWords(words);
     }
-    return counts;
   }
-//Method to split words and regenerate expressions
+
+  public int size() {
+    return counts.size();
+  }
+
+  public int total(){
+    return totalWords;
+  }
+
   String[] splitWords(String text) {
+
     return text.toLowerCase().split("[\\W_]+");
   }
 
@@ -57,4 +57,19 @@ public final class WordCounter {
         "counts=" + counts +
         '}';
   }
+
+  //Business/Helper Methods
+  //Method to countWords
+ void countWords(String[] words) {
+    for (String word : words){
+
+      //DONE CHECK if word is already present as a key in counts
+      // if its not presnet, add it to counts with a value of 1;
+      // otherwise, get the current value, add 1 to it , and update the map with the new value
+      counts.put(word, get(word) +1);
+      totalWords++;
+    }
+  }
+//Method to split words and regenerate expressions
+
 }
