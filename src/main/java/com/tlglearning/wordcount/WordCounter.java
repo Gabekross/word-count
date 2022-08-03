24ac1241d54393a86cdbf352d6bacc8215c1e762
Gabekross <olugabriel80@gmail.com>
2022-08-03 13:41:09 -0700

    Used maps in word counter

--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -1,11 +1,16 @@
 package com.tlglearning.wordcount;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class WordCounter {
+
+
+  private static final Set<String> BORING_WORDS = Set.of("and", "of", "the", "in",
+      "on", "i", "then", "than", "out", "a", "if");
 
   private final Map<String, Integer> counts = new HashMap<>();
 
@@ -55,15 +60,25 @@ public class WordCounter {
   //Business/Helper Methods
   //Method to countWords
  void countWords(String[] words) {
-    for (String word : words){
 
-      //DONE CHECK if word is already present as a key in counts
-      // if its not presnet, add it to counts with a value of 1;
-      // otherwise, get the current value, add 1 to it , and update the map with the new value
-      counts.put(word, get(word) +1);
-      totalWords++;
+   Arrays
+       .stream(words)
+       .map(String::trim)
+       .filter((s) -> !s.isEmpty())
+       .filter((word) -> word.length() > 5)
+       .filter((s) -> !BORING_WORDS.contains(s))
+//        .filter(Predicate.not(String::isEmpty))
+       .forEach((word) -> counts.put(word, 1 + counts.getOrDefault(word, 0)));
+
+//    for (String word : words){
+//
+//      //DONE CHECK if word is already present as a key in counts
+//      // if its not presnet, add it to counts with a value of 1;
+//      // otherwise, get the current value, add 1 to it , and update the map with the new value
+//      counts.put(word, get(word) +1);
+//      totalWords++;
     }
   }
 //Method to split words and regenerate expressions
 
-}
+
